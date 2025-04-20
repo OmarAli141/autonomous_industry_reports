565825464a7e2034c77d41c319e5e7fd73e3dbdd
Omar Ali <91081276+OmarAli141@users.noreply.github.com>
2025-04-20 17:15:39 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     # NewsAPI Configuration
-    NEWS_API_KEY = "4a8246110a18468098cbe066493f71f8"
+    NEWS_API_KEY = "SECURED"
     NEWS_API_URL = "http://newsapi.org/v2/everything"
     
     # Analysis Parameters
